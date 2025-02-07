d94fc2aec6e87cdff8b1519a573c264f705e3a60
Ramratan Gupta <ramratan.gupta@gmail.com>
2025-02-07 19:31:21 +0530

    no css guess

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,8 @@ const nextConfig: NextConfig = {
      unoptimized: true
    },
    // Remove the dot from basePath
-   basePath: process.env.NODE_ENV === 'production' ? '/' : '',
-   assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
+   basePath: process.env.NODE_ENV === 'production' ? '/_next' : '',
+   assetPrefix: process.env.NODE_ENV === 'production' ? '/_next' : '',
    trailingSlash: true,
 };
 
